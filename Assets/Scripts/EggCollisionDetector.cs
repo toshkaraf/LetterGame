99e30900582004b49b6013c1b9c476e4a1a3b004
Антон <rafes@ukr.net>
2016-10-06 14:59:50 +0300

    Basket is moved by arrows and detect eggs collisions

---
+++ Assets/Scripts/EggCollisionDetector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EggCollisionDetector : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        print("YAY!");
+        if (target.tag == "Egg")
+        {
+            target.gameObject.SetActive(false);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Players/Basket/Basket.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Basket : MonoBehaviour
+{
+
+    public float speed = 100f;
+    private Rigidbody2D myBody;
+
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        HandleKeyboard();
+    }
+
+    void HandleKeyboard()
+    {
+        float forceX = 0f;
+
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if (h > 0) forceX = speed;
+        else if (h < 0) forceX = -speed;
+
+        myBody.AddForce(new Vector2(forceX, 0));
+        //     transform.position.x = ;
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        print("YAY! basket");
+        if (target.tag == "Egg")
+        {
+            target.gameObject.SetActive(false);
+        }
+    }
+
+}
